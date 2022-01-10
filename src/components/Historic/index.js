3c197946daae7b8ed59103525660bf45b3ce53db
Rui Neto <ruineto11@gmail.com>
2022-01-10 16:06:32 -0300

    Added 50% the last bonus task

--- src/components/Historic/index.js
+++ src/components/Historic/index.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { Fragment, useState, useEffect, useContext } from "react";
-import moment from 'moment';
 import dayjs from 'dayjs';
 import TokenContext from "../../contexts/TokenContext";
 import PercentageContext from "../../contexts/PercentageContext";
@@ -8,13 +7,13 @@ import 'react-calendar/dist/Calendar.css';
 import Style from "./style";
 
 export default function Historic() {
-  const { Container, Content, Title, ContainerCalendar, StyledCalendar } = Style;
+  const { Container, Content, Title, ContainerCalendar, StyledCalendar, DateTitle, DateSubtitle } = Style;
   const { token } = useContext(TokenContext);
   const { percentage, setPercentage } = useContext(PercentageContext);
   const [historic, setHistoric] = useState(null);
+  const [selectedDayHistoric, setSelectedDayHistoric] = useState('');
   const [value, onChange] = useState(new Date());
   const [marked, setMarked] = useState([]);
-  /* const locale = "pt-BR"; */
 
   useEffect(() => {
     const promise = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/today', 
@@ -72,6 +71,70 @@ export default function Historic() {
     setMarked([ ...arrayMarked ]);
   }, [historic]);
 
+  function handleClickDay(date) {
+    const clickedDate = dayjs(date).format("DD/MM/YYYY");
+    console.log("clickedDate: ", clickedDate);
+
+    console.log("date(): ", dayjs(date).date());
+    console.log("month(): ", dayjs(date).month());
+    console.log("day(): ", dayjs(date).day());
+
+    let dateMonth = dayjs(date).date();
+    let month = dayjs(date).month();
+    let weekDay = dayjs(date).day();
+  
+    switch (weekDay) {
+      case 1:
+        weekDay = 'Segunda';
+        break;
+  
+      case 2:
+        weekDay = 'Terça';
+        break;
+  
+      case 3:
+        weekDay = 'Quarta';
+        break;
+  
+      case 4:
+        weekDay = 'Quinta';
+        break;
+  
+      case 5:
+        weekDay = 'Sexta';
+        break;
+  
+      case 6:
+        weekDay = 'Sábado';
+        break;
+  
+      case 0:
+        weekDay = 'Domingo';
+        break;
+    
+      default:
+        break;
+    }
+
+    const dateExtendend = `${weekDay}, ${dateMonth < 10 ? 0 + dateMonth.toString() : dateMonth}/${month + 1}`;
+
+    console.log(dateExtendend);
+
+    let habitsClickedDate = [];
+    for (let i = 0; i < historic.length; i++) {
+      const historicElement = historic[i];
+      
+      if(historicElement.day === clickedDate){
+        habitsClickedDate = [ ...historicElement.habits ];
+
+        break;
+      }
+    }
+
+    console.log("habitsClickedDate: ", habitsClickedDate);
+    setSelectedDayHistoric({"selectedDate": dateExtendend, "selectedDateHabits": [ ...habitsClickedDate ]});
+  }
+
   return(
     <Fragment>
       <Container>
@@ -82,15 +145,45 @@ export default function Historic() {
               onChange={onChange}
               value={value}
               tileClassName={({ date, view }) => {
-                if(marked.find(x => (x.date === moment(date).format("DD/MM/YYYY")) && x.completed === false)){
+                if(marked.find(x => (x.date === dayjs(date).format("DD/MM/YYYY")) && x.completed === false)){
                   return 'incompleted'
-                }else if(marked.find(x => (x.date === moment(date).format("DD/MM/YYYY")) && x.completed === true)){
+                }else if(marked.find(x => (x.date === dayjs(date).format("DD/MM/YYYY")) && x.completed === true)){
                   return 'completed'
                 }
               }}
               formatDay ={(locale, date) => dayjs(date).format('DD')}
+              onClickDay={(date) => handleClickDay(date)}
             />
           </ContainerCalendar>
+
+          <DateTitle>
+          { selectedDayHistoric !== '' ?
+              selectedDayHistoric.selectedDate
+            :
+            "Nenhum dia foi selecionado"
+          }
+          </DateTitle>
+          <DateSubtitle selectedDayHistoric={selectedDayHistoric}>
+            { selectedDayHistoric !== '' ?
+                selectedDayHistoric.selectedDateHabits.length > 0 ? 
+                  "x% dos hábitos completados"
+                :
+                  "Não há histórico para o dia selecionado"
+            : 
+              "Nenhum dia foi selecionado"
+            }
+          </DateSubtitle>
+
+          { selectedDayHistoric !== '' ?
+              selectedDayHistoric.selectedDateHabits.length > 0 &&
+                selectedDayHistoric.selectedDateHabits.map((currentHabit) => {
+                  return(
+                    <p key={currentHabit.id}>{currentHabit.id}</p>
+                  );
+                })
+            :
+              ""
+          }        
         </Content>
       </Container>
     </Fragment>
--- src/components/Historic/style.js
+++ src/components/Historic/style.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 
 const Container = styled.div`
   width: 100%;
-  height: 100%;
 
   padding-bottom: 120px;
 
@@ -27,7 +26,9 @@ const Title = styled.h2`
 `;
 
 const ContainerCalendar = styled.div`
-  height: 390px;
+  height: 402px;
+
+  margin-bottom: 30px;
 `;
 
 const StyledCalendar = styled(Calendar)`
@@ -40,12 +41,38 @@ const StyledCalendar = styled(Calendar)`
   border-radius: 10px;
 `;
 
+const DateTitle = styled.h2`
+  font-size: 22.976px;
+  line-height: 29px;
+
+  color: #126BA5;
+`;
+
+const DateSubtitle = styled.p`
+  font-size: 17.976px;
+  line-height: 22px;
+
+  margin-bottom: 28px;
+
+  color: 
+  ${
+    (props) => 
+      props.selectedDayHistoric !== '' &&
+        props.selectedDayHistoric.selectedDateHabits.length > 0 ?
+          "#8FC549" 
+        : 
+          "#BABABA"
+  };
+`;
+
 const components = {
   Container,
   Content,
   Title,
   ContainerCalendar,
-  StyledCalendar
+  StyledCalendar,
+  DateTitle,
+  DateSubtitle
 };
 
 export default components;
